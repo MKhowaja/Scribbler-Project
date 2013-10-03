7c5fddbf7cb700b3e5bbad294258633e57cadf04
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2013-10-03 13:33:51 -0400

    Update collisiondetect.py

--- src/collisiondetect.py
+++ src/collisiondetect.py
@@ -1,13 +1,58 @@
 from myro import *
 init("COM40")
 i = 0
-##loop to stop at the second obstacle seen
+sensor=[0,0,0]
 while i<100:
-    forward(1)
-    while getObstacle(1)>750:
-        turnRight(1)
+    sensor = getObstacle()
+    if sensor[0]<800 and sensor[1]<800 and sensor[2]<800:
+        forward(1)
+
+    elif sensor[0]>800 and sensor[2]<800:
+        turnRight(1, 0.3)
         
+    elif sensor[0]<800 and sensor[2]>800:
+        turnLeft(1, 0.3)
+
+    elif sensor[0]<800 and sensor[1]>800 and sensor[2]>800:
+        turnLeft(1, 0.3)
+
+    elif sensor[0]>800 and sensor[1]>800 and sensor[2]<800:
+        turnRight(1, 0.3)
+        
+    else: 
+        turnRight(1)
     i+=1
 stop()
 
-  
+
+##def timer(seconds=0):
+##     """ A function to be used with 'for' """
+##     start = time.time()
+##     while True:
+##         timepast = time.time() - start
+##         if seconds != 0 and timepast > seconds:
+##             raise StopIteration
+##         yield round(timepast, 3)
+## 
+## _timers = {}
+## def timeRemaining(seconds=0):
+##     """ Function to be used with 'while' """
+##     global _timers
+##     if seconds == 0: return True
+##     now = time.time()
+##     stack = traceback.extract_stack()
+##     filename, line_no, q1, q2 = stack[-2]
+##     if filename.startswith("<pyshell"):
+##         filename = "pyshell"
+##     if (filename, line_no) not in _timers:
+##         _timers[(filename, line_no)] = (now, seconds)
+##         return True
+##     start, duration = _timers[(filename, line_no)]
+##     if seconds != duration:
+##         _timers[(filename, line_no)] = (now, seconds)
+##         return True
+##     if now - start > duration:
+##         del _timers[(filename, line_no)]
+##         return False
+##     else:
+##         return True  
