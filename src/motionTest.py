50bb9e744c7db1bd23d5c48a23e9ec302a42febd
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2013-10-03 13:34:57 -0400

    Create motionTest.py

---
+++ src/motionTest.py
@@ -0,0 +1,11 @@
+from myro import *
+init("COM40")
+
+forward(1, 1)
+turnRight(0.5, 0.25)
+forward(1, 1.5)
+turnLeft(0.5, 0.25)
+turnRight(0.5, 0.25)
+backward(1, 1.5)
+turnLeft(0.5, 0.25)
+backward(1, 1)
