a8fe7ffbbc3d058074d4c6b16a2a6ad1c0f6fb69
KevXman <kevinxiao2@gmail.com>
2013-10-03 12:06:58 -0400

    Update collisiondetect.py

--- src/collisiondetect.py
+++ src/collisiondetect.py
@@ -1,13 +1,13 @@
 from myro import *
 init("COM40")
 def main():
-    int i = 0
+    i = 0
     ##loop to stop at the second obstacle seen
     while(i<2):
         Forward(1,1)
         if (getObstacle(1)>800):
             turnRight(1,1)
-            i++
+            i+=1
 
         stop()
 
