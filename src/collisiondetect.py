d9eb0cc992da2849de793d38025561ee6ef8ed1c
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2013-10-03 11:52:23 -0400

    Update collisiondetect.py

--- src/collisiondetect.py
+++ src/collisiondetect.py
@@ -1,13 +1,12 @@
 from myro import *
 init("COM40")
 def main():
-    ##rSetForwardnessTxt("fluke-forward");
     int i = 0;
     ##loop to stop at the second obstacle seen
     while(i<2):
         Forward(1,1)
-        if (rGetObstacleNum(1)>800):
-            rTurnRight(1,1)
+        if (getObstacle(1)>800):
+            turnRight(1,1)
             i++
 
         stop()
