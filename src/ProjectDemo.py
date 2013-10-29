4fb037564fafd8b61b2b5cb6a61f2606748c4bcc
KevXman <kevinxiao2@gmail.com>
2013-10-29 12:27:25 -0400

    Added procode

--- src/ProjectDemo.py
+++ src/ProjectDemo.py
@@ -2,65 +2,52 @@ from myro import *
 import math
 
 init("COM40")
-i = 0
 j = 0
-SPEED = 10.6 #m/s
+SPEED = 10.6 #cm/s
 ANGLE = X #degrees/s
 sensor=[0,0,0]
 time = 0 #time travelled in current heading
-ROTATE_EXTRA = ANGLE*1.2 ## to account for robot size, turn more than what the sensors detect to completely avoid object
+ROTATE_EXTRA = ANGLE*T #T is some time TBD
 
-destination = [int(raw_input), int(raw_input)]
-current = [0.0, 0.0]
-left = 0
-right = 0
-offset_angle = 0
-direct_angle= math.atan(destination[1]/destination[0])
-current_angle = 0
-turnToHeading(direct_angle, current_angle, ANGLE);
+destination = [int(raw_input)*100, int(raw_input)*100]  #destination coordinates (convert to cm)
+current = [0.0, 0.0]    #current coordinates (will be incremented by cm)
+obstacle = 0    #variable to see if robot is moving past an obstacle
+direct_angle = math.atan(destination[1]/destination[0])  #angle towards destination
+current_angle = 0   #angle the robot is facing
+turnToHeading(direct_angle, current_angle, ANGLE)
 
-while math.fabs(current[0]-destination[0]) >= 5 and math.fabs(current[1]-destination[1]) >= 5:
-    direct_angle=math.atan((destination[1]-current[1])/(destination[0]-current[0]))
-    
+while math.fabs(current[0]-destination[0]) >= 5 and math.fabs(current[1]-destination[1]) >= 5: #move until scribby is 5cm away
+    direct_angle=math.atan((destination[1]-current[1])/(destination[0]-current[0]))     #Track angle towards destination
     sensor = getObstacle()
-    #if sensor[0]<800 and sensor[1]<800 and sensor[2]<800:
-    forward(1)
-
-    if sensor[0]>=sensor[2]:
+    if sensor[0]<500 and sensor[1]<500 and sensor[2]<500:   #If path is clear, move forward and track coordinates
+        forward(1)
+        current[0] += SPEED*math.cos(current_angle)
+        current[1] += SPEED*math.sin(current_angle)
+        if obstacle == 1         #If it just turned from an obstacle, try to turn back towards destination
+            obstacle = 0
+            turnToHeading(direct_angle, current_angle, ANGLE)
+    elif sensor[0]>=sensor[2]:      #If obstacle to left, turn right and track angle
         turnRight(1)
         current_angle -= math.radians(ANGLE)
-        left=1
-        if sensor[0]<sensor[2]:         ##Turn a bit more to account for robot size
+        obstacle = 1
+        if sensor[0]<sensor[2]:         #Turn a bit more to account for robot size
             for j in range(0, ROTATE_EXTRA)
                 turnRight(1)
                 current_angle-=math.radians(ANGLE)
-        
-            
-        
-    if sensor[2]>sensor[0]: 
+    elif sensor[2]>sensor[0]:       #If obstacle to right, turn left and track angle
         turnLeft(1)
         current_angle += math.radians(ANGLE)
-        right = 1
-        if sensor[2]>=sensor[0]:         ##Turn a bit more to account for robot size
+        obstacle = 1
+        if sensor[2]>=sensor[0]:         #Turn a bit more to account for robot size
             for j in range(0, ROTATE_EXTRA)
                 turnLeft(1)
                 current_angle += math.radians(ANGLE)
-
-    current[0] += SPEED*math.cos(current_angle)
-    current[1] += SPEED*math.sin(current_angle)
-    
-    i+=1
 stop()
 
-def turnToHeading (direct_angle, current_angle, ANGLE):
+def turnToHeading (direct_angle, current_angle, ANGLE):     #turn robot towards destination
     if direct_angle>current_angle:
         turnRight((direct_angle-current_angle)/math.radians(ANGLE))
         current_angle=direct_angle
     elif direct_angle<current_angle:
         turnLeft((current_angle-direct_angle)/math.radians(ANGLE))
         current_angle=direct_angle
-        
-
-##def checkLeftObstacle:
-    ##turnRight(90 degrees)
-#    checkSensors()
