8ff8bd2b4964f3f7c87f746faf97c01b45df342e
MrScarecrow <shreykhosla5@gmail.com>
2013-11-04 19:43:58 -0500

    "collision5" - finished

--- src/ProjectDemo.py
+++ src/ProjectDemo.py
@@ -1,59 +1,132 @@
+# Problem: center is x, y, not front of robot (cause when rotating that gets screwed up)
+# TODO: use camera in decision making
+# to do: check if it's continually getting further from the destination
+
 from myro import *
 import math
 
 init("COM5")
-i = 0
-j = 0
-SPEED = 10.6 #cm/s
-ANGLE = 115.3 #degrees/s
-sensor=[0,0,0]
-time = 0 #time travelled in current heading
-ROTATE_EXTRA = ANGLE*0.06
 
-a = int(raw_input("Enter x distance (m): "))
-b = int(raw_input("Enter y distance (m): "))
-destination = [a*100, b*100] #destination coordinates (convert to cm)
-current = [0.0, 0.0] #current coordinates (will be incremented by cm)
-obstacle = 0 #variable to see if robot is moving past an obstacle
-direct_angle = math.atan(destination[1]/destination[0]) #angle towards destination
-current_angle = 0 #angle the robot is facing
-turnToHeading(direct_angle, current_angle, ANGLE)
+# position
+x = 0
+y = 0
+theta = 90
 
-while math.fabs(current[0]-destination[0]) >= 5 and math.fabs(current[1]-destination[1]) >= 5: #move until scribby is 5cm away
-    direct_angle=math.atan((destination[1]-current[1])/(destination[0]-current[0])) #Track angle towards destination
-    sensor = getObstacle()
-    if sensor[0]<500 and sensor[1]<500 and sensor[2]<500: #If path is clear, move forward and track coordinates
-        forward(1, 0.1)
-        current[0] += SPEED*math.cos(current_angle)/10
-        current[1] += SPEED*math.sin(current_angle)/10
-        if obstacle == 1: #If it just turned from an obstacle, try to turn back towards destination
-            if i==10
-                i=0
-                obstacle = 0
-                turnToHeading(direct_angle, current_angle, ANGLE)
-            i++
-    elif sensor[0]>=sensor[2]: #If obstacle to left, turn right and track angle
-        turnRight(1, 0.1)
-        current_angle -= math.radians(ANGLE)/10
-        obstacle = 1
-        if sensor[0]<sensor[2]: #Turn a bit more to account for robot size
-            for j in range(0, ROTATE_EXTRA*10):
-                turnRight(1, 0.1)
-                current_angle-=math.radians(ANGLE)/10
-    elif sensor[2]>sensor[0]: #If obstacle to right, turn left and track angle
-        turnLeft(1, 0.1)
-        current_angle += math.radians(ANGLE)/10
-        obstacle = 1
-        if sensor[2]>=sensor[0]: #Turn a bit more to account for robot size
-            for j in range(0, ROTATE_EXTRA*10):
-                turnLeft(1, 0.1)
-                current_angle += math.radians(ANGLE)/10
-stop()
+# destination
+x_f = 0
+y_f = 200
+r_f = 5 # radius of endzone
 
-def turnToHeading (direct_angle, current_angle, ANGLE): #turn robot towards destination
-    if direct_angle>current_angle:
-        turnRight(1, (direct_angle-current_angle)/math.radians(ANGLE))
-        current_angle=direct_angle
-    elif direct_angle<current_angle:
-        turnLeft(1, (current_angle-direct_angle)/math.radians(ANGLE))
-        current_angle=direct_angle
+### CONSTANTS ###
+ir_rng = 13 # range of the IR sensor (cm)
+threshold = 1050 # threshold for IR obstacle
+v = 12.5 # velocity (cm/s)
+w = 115.38 # omega (degrees/s)
+
+def getDeltaTheta(): # returns the angle between where it's facing and where it needs to go
+	global x, y, theta
+	d_x = x_f - x
+	d_y = y_f - y
+	if d_x == 0:
+            if(d_y > 0):
+		d_theta = 90
+	    else:
+                d_theta = 270
+	else:
+		d_theta = math.degrees(math.atan(d_y/d_x))
+	print "Delta theta calculated to be:%d" % (d_theta - theta)
+        if(d_x >= 0):
+            return (d_theta - theta)
+        else:
+            return (d_theta + theta)
+
+def turn(n): # rotates n degrees, updates theta
+	global w, theta
+	print "Turning %d degrees (%d seconds)..." % (n, abs(n/w))
+	if n >= 0:
+		turnLeft(1, n/w)
+	else:
+		turnRight(1, -n/w)
+	theta += n
+
+def obstructed(n): # faces angle n, checks whether obstructed, turns back
+	global threshold
+	obstructed = False
+	if n != 0:
+		turn(n)
+	s = getObstacle()
+	print "Obstacles:%s, threshold:%d" %(str(s), threshold)
+	if s[0] > threshold or s[1] > threshold or s[2] > threshold:
+		obstructed = True
+	if obstructed:
+		print "Obstructed!"
+	if n != 0:
+		turn(-n)
+	return obstructed
+
+def move(d): # moves d centimeters, updates position
+	global x, y, theta, v
+	print "Moving %d cm (%f seconds)..."%(d, abs(d/v))
+	if d >= 0:
+		forward(1, d/v)
+	else:
+		backward(1, -d/v)
+
+	print "Initial position: x:%d y:%d theta:%d" % (x, y, theta)
+	x += d*math.cos(math.radians(theta)) # update x
+	y += d*math.sin(math.radians(theta)) # update y
+	print "Final position: x:%d y:%d theta:%d" % (x, y, theta)
+
+def atDest():
+	return abs(x_f - x) < r_f and abs(y_f - y) < r_f
+
+# until it reaches destination
+while not atDest():
+	# rotate to face destination
+	angle = getDeltaTheta()
+	turn(angle)
+
+	# move forward until detects obstacle
+	while not obstructed(0) and not atDest():
+		raw_input("Press any key to continue...")
+		move(ir_rng)
+
+	if atDest():
+		break
+
+	# turn based on which sensor detects the obstacle
+	if getObstacle(0) > getObstacle(2):
+		angle_turned = -90
+	else:
+		angle_turned = 90
+	raw_input("Press any key to continue...")
+	turn(angle_turned)
+
+	raw_input("Press any key to continue...")
+
+	move(ir_rng)
+
+	# while its route back to the path is obstructed
+	while obstructed(-angle_turned):
+		raw_input("Press any key to continue...")
+		if obstructed(0):
+			turn(angle_turned)
+		else:
+			move(ir_rng)
+
+	move(ir_rng) # move once more for safety
+	turn(-angle_turned) # get back on path
+
+	move(ir_rng) # move once more for safety
+
+	# while its route back to the path is obstructed
+	while obstructed(-angle_turned):
+		raw_input("Press any key to continue...")
+		if obstructed(0):
+			turn(angle_turned)
+		else:
+			move(ir_rng)
+	
+	move(ir_rng) # move once more for safety
+
+	raw_input("Press any key to continue...")
