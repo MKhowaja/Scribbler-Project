9f2023da9a5965725ed96421568a3c85b18a0c4c
Clement Hoang <clement.hoang@uwaterloo.ca>
2013-11-23 14:12:42 -0500

    isCustomer function

---
+++ src/isCustomer.py
@@ -0,0 +1,16 @@
+#generally called when the Scribbler meets an obstacle
+#returns True if the obstacle if a customer; returns False otherwise
+#customer is an obstacle if more than a quarter of the picture is red/pink
+def IsCustomer():
+    obstacle = takePicture("color")
+    counter = 0
+    for i in range(getWidth(picture)):
+        for j in range(getHeight(picture)):
+            pixel = getPixel(picture, i, j)
+            #if pixel's colour is in range of colour (pink/red in this case)
+            if getRed(pixel)==255:
+                counter++
+    if counter >= (getWidth(picture)*getHeight(picture)/4)
+        return True
+    else
+        return False
