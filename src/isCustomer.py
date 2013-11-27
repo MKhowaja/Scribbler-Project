a6f633ba99c187cebbde60eb0916b19b39f55e62
KevXman <kevinxiao2@gmail.com>
2013-11-27 18:04:13 -0500

    Get that colour better, make the reading easier.

--- src/isCustomer.py
+++ src/isCustomer.py
@@ -2,15 +2,17 @@
 #returns True if the obstacle if a customer; returns False otherwise
 #customer is an obstacle if more than a quarter of the picture is red/pink
 def IsCustomer():
-    obstacle = takePicture("color")
-    counter = 0
-    for i in range(getWidth(picture)):
+    picture = takePicture("color")  #takes picture of obstacle
+    counter = 0 #variable for number of red 
+    #loop for every pixel in photo
+    for i in range(getWidth(picture)): 
         for j in range(getHeight(picture)):
-            pixel = getPixel(picture, i, j)
-            #if pixel's colour is in range of colour (pink/red in this case)
-            if getRed(pixel)>200:
-                counter++
-    if counter >= (getWidth(picture)*getHeight(picture)/4)
+            pixel = getPixel(picture, i, j) #read pixel
+            #if pixel's colour is in range of colour (red)
+            if getRed(pixel)>200 and getBlue(pixel)<55 and getGreen(pixel)<55:
+                counter++ #add one to count
+    #if more than 2/3 of photo matches colour
+    if counter >= (2*getWidth(picture)*getHeight(picture)/3)
         return True
     else
         return False
