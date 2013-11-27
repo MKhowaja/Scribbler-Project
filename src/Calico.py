982602d5201514bf55434a335c43c0dfc0d99c34
MrScarecrow <shreykhosla5@gmail.com>
2013-11-27 18:10:24 -0500

    commented

--- src/Calico.py
+++ src/Calico.py
@@ -1,4 +1,3 @@
-# Problem: center is x, y, not front of robot (cause when rotating that gets screwed up)
 # TODO: use camera in decision making
 # to do: check if it's continually getting further from the destination
 
@@ -8,12 +7,16 @@
     #instead of turning 90, turn to face destination
     #get velocity by doing move-by
 
+# to do:
+    # color recognition
+    # music
+
 from Myro import *
 import math
 
 init("COM7")
 
-# position
+# initial position
 x = 0
 y = 0
 theta = 90
@@ -25,14 +28,17 @@ theta_f = 90
 r_f = 10 # radius of endzone
 
 ### CONSTANTS ###
-ir_rng = 18 # range of the IR sensor (cm)
-threshold = 1000 # threshold for IR obstacle
+ir_rng = 18 # range of the IR sensor (cm) (how far away it will notice an object)
+threshold = 1000 # threshold for IR obstacle (anything greater is considered blocked)
 v = 12.5 # velocity (cm/s)
-w = 115.38 # omega (degrees/s)
+#w = 115.38 # angular speed (omega) (degrees/s)
+
+# setup
 setIRPower(132)
 setS2Volume(100)
 
-def getDeltaTheta(theta): # returns the angle between where it's facing and where it needs to go
+# Returns how much you need to turn by to face the final destination from the given angle
+def getDeltaTheta(theta):
     global x, y
     d_x = x_f - x
     d_y = y_f - y
@@ -44,28 +50,34 @@ def getDeltaTheta(theta): # returns the angle between where it's facing and wher
     else:
         d_theta = math.degrees(math.atan(d_y/d_x))
 
-    if(d_x < 0):
+    if(d_x < 0): # quandrant adjustment
         if(d_y < 0):
             d_theta -= 180
         else:
             d_theta += 180
 
     if(d_x >= 0):
-        d_theta_f = d_theta - theta
+        d_theta = d_theta - theta
     else:
-        d_theta_f = d_theta + theta
+        d_theta = d_theta + theta
 
-    print ("Delta theta calculated to be:", d_theta_f)
+    print ("Delta theta calculated to be:", d_theta)
 
-    return d_theta_f
+    return d_theta
 
-def turn(n): # rotates n degrees, updates theta
+# Rotates n degrees, updates theta
+def turn(n):
     global theta
     print ("Turning %d degrees..." % n)
     turnBy(n)
     theta += n
 
-def obstruct():
+# Faces angle n, checks whether obstructed, turns back
+def obstructed(n):
+    global threshold
+    if n != 0:
+        turn(n)
+
     obstructed = False
 
     num_readings = 8
@@ -75,28 +87,20 @@ def obstruct():
     s.sort()
     reading = s[int(num_readings/2 - 1)]
 
-    print ("Reading:%s, threshold:%d" %(reading, threshold))
     if reading >= threshold:
         obstructed = True
-    if obstructed:
-        print ("Obstructed!")
 
-    return obstructed
-
-def obstructed(n): # faces angle n, checks whether obstructed, turns back
-    global threshold
-    if n != 0:
-        turn(n)
-
-    blocked = False
-    blocked = obstruct()
+    print ("Reading:%s, threshold:%d, obstructed?:%b" %(reading, threshold, obstrcuted))
 
     if n != 0:
         turn(-n)
-    return blocked
 
-def move(d): # moves d centimeters, updates position
+    return obstructed
+
+# Moves d centimeters, updates position
+def move(d):
     global x, y, theta, v
+    print ("Position before moving: x:%d y:%d theta:%d" % (x, y, theta))
     print ("Moving %d cm (%f seconds)..."%(d, abs(d/v)))
     if d >= 0:
         direction = 1
@@ -114,13 +118,17 @@ def move(d): # moves d centimeters, updates position
 
     motors(0,0)
 
-    print ("Initial position: x:%d y:%d theta:%d" % (x, y, theta))
     x += travelled*math.cos(math.radians(theta)) # update x
     y += travelled*math.sin(math.radians(theta)) # update y
-    print ("Final position: x:%d y:%d theta:%d" % (x, y, theta))
+    print ("Position after moving: x:%d y:%d theta:%d" % (x, y, theta))
 
+# Returns whether within radius of endzone
 def atDest():
     return abs(x_f - x) < r_f and abs(y_f - y) < r_f
+
+#### MAIN LOOP ####
+x_f = int(raw_input("Enter final x:"))
+y_f = int(raw_input("Enter final y:"))
 
 # until it reaches destination
 while not atDest():
@@ -132,15 +140,16 @@ while not atDest():
     move(math.sqrt((x_f-x)**2+(y_f-y)**2))
 
     if atDest():
+        print("Arrived at destination")
         turn(theta_f - theta)
         beep(1,880)
         break
 
     # turn based on which sensor detects the obstacle
-    if getObstacle(0) > getObstacle(2):
-        angle_turned = -90
-    else:
+    if getObstacle(0) < getObstacle(2):
         angle_turned = 90
+    else:
+        angle_turned = -90
 
     #raw_input("Press any key to continue...")
     turn(angle_turned)
@@ -157,8 +166,8 @@ while not atDest():
             move(ir_rng)
 
     move(ir_rng) # move once more for safety
-    turn(-angle_turned) # get back on path
-    move(ir_rng*2) # move once more for safety
+    turn(-angle_turned) # turn back on path
+    move(ir_rng*2) # move past obstacle
 
     # while its route back to the path is obstructed
     while obstructed(-angle_turned):
@@ -169,5 +178,4 @@ while not atDest():
             move(ir_rng)
     
     move(ir_rng) # move once more for safety
-
     #raw_input("Press any key to continue...")
